9b2a0b1a85ad1ed423b79d9d6099f2be57248bfd
Aadharsh S Alappatt <alappattsaadharsh@gmail.com>
2023-04-10 21:30:26 +0530

    details

---
+++ tutor/details.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Details</title>
+    <link rel="stylesheet" href="details.css">
+</head>
+<body>
+<ul>
+      <li><a class="active" href="#">Details</a></li>
+      <li><a href="tutorhome.php">Home</a></li>
+      <li style="float:right; "><a href="../student/logout.php" class="logout">Logout</a></li>
+</ul>
+</body>
+</html>
