d2321b3a678d722d6a36e50b35e4b099c141d6a2
Madhusmita <madhumukh2002@gmail.com>
2023-04-10 23:18:52 +0530

    quiz module update

---
+++ Quiz/adminpanel/admin/includes/sidebar.php
@@ -0,0 +1,128 @@
+   <div class="app-sidebar sidebar-shadow" style="background-color: #454545;">
+                    <!-- <div class="app-header__logo">
+                        <div class="logo-src"></div>
+                        <div class="header__pane ml-auto">
+                            <div>
+                                <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
+                                    <span class="hamburger-box">
+                                        <span class="hamburger-inner"></span>
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div> -->
+                    <div class="app-header__mobile-menu">
+                        <div>
+                            <button type="button" class="hamburger hamburger--elastic mobile-toggle-nav">
+                                <!-- <span class="hamburger-box"> -->
+                                    <!-- <span class="hamburger-inner"></span> -->
+                                <!-- </span> -->
+                            </button>
+                        </div>
+                    </div>
+                    <div class="app-header__menu">
+                        <span>
+                            <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                                <!-- <span class="btn-icon-wrapper"> -->
+                                    <!-- <i class="fa fa-ellipsis-v fa-w-6"></i> -->
+                                <!-- </span> -->
+                            </button>
+                        </span>
+                    </div>    
+                    <div class="scrollbar-sidebar">
+                        <div class="app-sidebar__inner">
+                            <ul class="vertical-nav-menu">
+                                <li class="app-sidebar__heading"><a href="home.php"> </a></li>
+
+                                <li class="app-sidebar__heading" style="color: #4FFFCA;">MANAGE COURSE</li>
+                                <li>
+                                    <a href="#" style="color: #f1f4f6;">
+                                         <i class="metismenu-icon pe-7s-display2"></i>
+                                         Course
+                                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                    </a>
+                                    <ul>
+                                        <li>
+                                            <a href="#" data-toggle="modal" data-target="#modalForAddCourse" style="color: #f1f4f6;">
+                                                <i class="metismenu-icon"></i>
+                                                Add Course
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="home.php?page=manage-course" style="color: #f1f4f6;">
+                                                <i class="metismenu-icon">
+                                                </i>Manage Course
+                                            </a>
+                                        </li>
+                                       
+                                    </ul>
+                                </li>
+                               
+                                <li class="app-sidebar__heading" style="color: #4FFFCA;">MANAGE QUIZ</li>
+                                <li>
+                                    <a href="#" style="color: #f1f4f6;">
+                                         <i class="metismenu-icon pe-7s-display2"></i>
+                                         Quiz
+                                        <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
+                                    </a>
+                                    <ul>
+                                        <li>
+                                            <a href="#" data-toggle="modal" data-target="#modalForExam" style="color: #f1f4f6;">
+                                                <i class="metismenu-icon"></i>
+                                                Add Quiz
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="home.php?page=manage-exam" style="color: #f1f4f6;">
+                                                <i class="metismenu-icon">
+                                                </i>Manage Quiz
+                                            </a>
+                                        </li>
+                                       
+                                    </ul>
+                                </li>
+                           
+                         
+                                <!-- <li class="app-sidebar__heading">MANAGE EXAMINEE</li>
+                                <li>
+                                    <a href="" data-toggle="modal" data-target="#modalForAddExaminee">
+                                        <i class="metismenu-icon pe-7s-add-user">
+                                        </i>Add Examinee
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="home.php?page=manage-examinee">
+                                        <i class="metismenu-icon pe-7s-users">
+                                        </i>Manage Examinee
+                                    </a>
+                                </li> -->
+                                <li class="app-sidebar__heading" style="color: #4FFFCA;">RANKING</li>
+                                <li>
+                                    <a href="home.php?page=ranking-exam" style="color: #f1f4f6;">
+                                        <i class="metismenu-icon pe-7s-cup">
+                                        </i>Ranking By Quiz
+                                    </a>
+                                </li>
+
+
+                                <li class="app-sidebar__heading" style="color: #4FFFCA;">REPORTS</li>
+                                <li>
+                                    <a href="home.php?page=examinee-result" style="color: #f1f4f6;">
+                                        <i class="metismenu-icon pe-7s-cup">
+                                        </i>Student Result
+                                    </a>
+                                </li>
+                              
+
+                                 <!-- <li class="app-sidebar__heading">FEEDBACKS</li>
+                                <li>
+                                    <a href="home.php?page=feedbacks">
+                                        <i class="metismenu-icon pe-7s-chat">
+                                        </i>All Feedbacks
+                                    </a>
+                                </li> -->
+                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>  
