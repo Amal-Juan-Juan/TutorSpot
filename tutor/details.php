2d1136c6bf54cdf637d1d0ab3b67abc3089868e7
Aadharsh S Alappatt <alappattsaadharsh@gmail.com>
2023-04-12 20:57:52 +0530

    details

--- tutor/details.php
+++ tutor/details.php
@@ -16,5 +16,21 @@ session_start();
       <li><a href="tutorhome.php">Home</a></li>
       <li style="float:right; "><a href="../student/logout.php" class="logout">Logout</a></li>
 </ul>
+
+<div class="section">
+        <h1>View Course Details</h1>
+    </div>
+<div id="formdiv1" class="formdiv" style="margin-top:10px;"><br />
+		
+      <form id="formin" method="post" enctype="multipart/form-data">
+      <label class="inputl">Course Name</label>
+        <input type="text" name="coursename" class="inputf" size="90px" required/><br />
+        <label class="inputl" >Tutor ID</label>
+        <input type="text" name="tutorid" class="inputf" size="90px" required/><br />
+        
+        
+				<input style="font-size: 1.5em" id="subm" type="submit" name="submit" value="View">
+      </form>
+    </div>
 </body>
 </html>
