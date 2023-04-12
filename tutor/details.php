a6fc807e148e8ac2cce88df089ac099452903669
Aadharsh S Alappatt <alappattsaadharsh@gmail.com>
2023-04-12 21:45:03 +0530

    student,tutor done

--- tutor/details.php
+++ tutor/details.php
@@ -32,7 +32,7 @@ session_start();
 				<input style="font-size: 1.5em" id="subm" type="submit" name="submit" value="View">
       </form>
     </div>
-    
+    <br><br><br>
 <?php
 $host = "localhost";  
 $user = "root";  
@@ -50,12 +50,42 @@ if(isset($_POST['submit']))
   $coursename=$_POST['coursename'];
   
 
-  $query="select coursereg.username,coursereg.email,studentdetails.name,studentdetails.age,studentdetails.gender,studentdetails.phone, studentdetails.yearOfStudy,studentdetails.location,studentdetails.university from coursereg inner join studentdetails on coursereg.email=studentdetails.email where coursereg.coursename='$coursename' and coursereg.tutorid='$tutorid';";
+  $query="select studentdetails.name,coursereg.email,studentdetails.age,studentdetails.gender,studentdetails.phone, studentdetails.yearOfStudy,studentdetails.location,studentdetails.university from coursereg inner join studentdetails on coursereg.email=studentdetails.email where coursereg.coursename='$coursename' and coursereg.tutorid='$tutorid';";
   $result1=mysqli_query($con,$query);
   $count=mysqli_num_rows($result1);
   if($count>0)
   {
+    $i=1;
+    echo "<div class='div1'>";
+    echo "<table>";
+    echo "<tr id='header'>";
+    echo "<td>S.No</td>";
+    echo "<td> Student Name</td>";    
+    echo "<td> Student Email</td>";
+    echo "<td> Age</td>";
+    echo "<td> Gender</td>";
+    echo "<td> Phone</td>";
+    echo "<td> Year Of Study</td>";
+    echo "<td> Location</td>";
+    echo "<td> University</td>";
+    echo "</tr>";
     
+  while($row = mysqli_fetch_array($result1)){
+      echo "<tr>";
+      echo "<td>".$i++."</td>";
+      echo "<td>".$row[0]."</td>";
+      echo "<td>".$row[1]."</td>";
+      echo "<td>".$row[2]."</td>";
+      echo "<td>".$row[3]."</td>";
+      echo "<td>".$row[4]."</td>";
+      echo "<td>".$row[5]."</td>";
+      echo "<td>".$row[6]."</td>";
+      echo "<td>".$row[7]."</td>";
+      echo "</tr>";
+  } 
+  
+    echo "</table>";
+    echo "</div>";
         
   }
   else {
